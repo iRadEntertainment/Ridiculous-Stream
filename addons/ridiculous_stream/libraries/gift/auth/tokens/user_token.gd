0269191a8e78469059b3e0fdeca058b665da9800
Dario De Vita <devitadario@gmail.com>
2024-04-04 21:21:32 +0200

    refactor(Gift): Gift library removed from the project

--- addons/ridiculous_stream/libraries/gift/auth/tokens/user_token.gd
+++
@@ -1,8 +0,0 @@
-class_name UserAccessToken
-extends TwitchToken
-
-var scopes : PackedStringArray
-
-func _init(data : Dictionary, client_id : String) -> void:
-	super._init(data, client_id)
-	scopes = data["scope"]
