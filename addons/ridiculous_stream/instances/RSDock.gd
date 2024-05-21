1f8bfe7811d0d6ef5fb560085e3fc95afeca3afa
Dario De Vita <devitadario@gmail.com>
2024-05-21 17:30:48 +0200

    chore: project.godot backup before importing the new library from kani

--- addons/ridiculous_stream/instances/RSDock.gd
+++ addons/ridiculous_stream/instances/RSDock.gd
@@ -5,24 +5,22 @@
 extends PanelContainer
 class_name RSDock
 
-var main: RSMain
-@onready var pnl_mods = %pnl_mods
-@onready var pnl_quick_actions = %pnl_quick_actions
-@onready var pnl_co_pilot = %pnl_co_pilot
-@onready var pnl_chat = %pnl_chat
+var main : RSMain
 
 
 func start():
 	connect_twitcher_signals()
-	for pnl in [pnl_chat, pnl_co_pilot, pnl_quick_actions, pnl_mods]:
+	for pnl in [%pnl_chat, %pnl_co_pilot, %pnl_quick_actions]:
 		pnl.main = main
 		pnl.start()
+
 
 func connect_twitcher_signals():
 	if !main.twitcher.received_chat_message.is_connected(play_notification):
 		main.twitcher.received_chat_message.connect(play_notification)
 	if !main.twitcher.connected_to_twitch.is_connected(start):
 		main.twitcher.connected_to_twitch.connect(start)
+
 
 func play_notification(_channel_name: String, username: String, _message: String, _tags: TwitchTags.PrivMsg):
 	if !get_parent(): return
@@ -32,14 +30,9 @@ func play_notification(_channel_name: String, username: String, _message: String
 	get_parent().add_child(new_notif_inst)
 	new_notif_inst.play()
 
+
 func _on_btn_reload_pressed():
 	main.call_deferred("reload_dock")
 # TEST: remove test stuff
 func _on_btn_test_stuff_pressed():
-	for user in main.known_users:
-		print(main.known_users.display_name)
-	#var followers : int = await main.twitcher.get_follower_count()
-	#var mods : Array[TwitchUserModerator] = await main.twitcher.get_moderators()
-	#print("Followers: ", followers)
-	#print("Mods: ", mods.size())
-	
+	main.custom.add_commands()
--- addons/ridiculous_stream/instances/pnl_settings.gd
+++ addons/ridiculous_stream/instances/pnl_settings.gd
@@ -14,9 +14,7 @@ func start():
 	connect_tab_buttons_to_tab_container()
 	call_deferred("update_sidebar_pos")
 	resized.connect(update_sidebar_pos)
-	#about_to_popup.connect(reload_panels)
 	%pnl_twitch_user_list.user_selected.connect(%pnl_twitch_user_fields.populate_fields)
-	#%pnl_twitch_user_fields.new_user_file.connect(populate)
 
 
 func populate():
@@ -74,7 +72,7 @@ func _on_btn_open_github_pressed():
 	OS.shell_open("https://github.com/iRadEntertainment/Ridiculous-Stream")
 	main.twitcher.chat("Here is the repo for Ridiculous Stream, feel free to use, modify or contribute: https://github.com/iRadEntertainment/Ridiculous-Stream")
 func _on_btn_credits_gift_pressed():
-	main.twitcher.chat("Issork made Gift! That's what I am using: https://github.com/issork/gift")
+	main.twitcher.chat("Issork made Gift! The library I used before the Twitcher: https://github.com/issork/gift")
 func _on_btn_credits_twitcher_pressed():
 	main.twitcher.chat("Kanimaru made Twitcher! https://github.com/kanimaru/twitcher")
 func _on_btn_credits_noobs_pressed():
